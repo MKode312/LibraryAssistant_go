11814ff13e1cf9fcd079c08a7eaedb9e806d6692
Mark <mark@MacBook-Air-Mark.local>
2026-03-04 21:04:51 +0500

    patched issue microservice

--- LibAssistant-sso/cmd/migrator/main.go
+++ LibAssistant-sso/cmd/migrator/main.go
@@ -36,7 +36,7 @@ func main() {
 		panic(err)
 	}
 
-	if err := m.Down(); err != nil {
+	if err := m.Up(); err != nil {
 		if errors.Is(err, migrate.ErrNoChange) {
 			fmt.Println("no migrations to apply")
 
